f20c1b91898a7f584b3efeef69ea19cef420e7f1
iqrahafeez-2 <146338521+iqrahafeez-2@users.noreply.github.com>
2023-11-02 16:43:57 +0500

    Add files via upload

---
+++ source iii.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<ctime>
+#include<string>
+using namespace std;
+int main()
+{
+	int books;
+	int issuedate;
+	int returndate;
+	int fine;
+	int daysforbooks = 6;
+	int choice;
+	int choice" ";
+	
+	cout << "1;bookborrowed";
+	cout << "2.bookreturned";
+	while (choice== 2) {
+		cin >> choice;
+		if(books>0){
+			cout << "you can borrow book";
+		else{
+			cout<<" "
+}
